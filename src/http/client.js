5ecc2611896686d26f4109c812d0b7bcffa0b101
zzydev <4amzzydev@gmail.com>
2023-06-13 13:46:07 +0800

    feat:  Request

--- src/http/client.js
+++ src/http/client.js
@@ -1,5 +1,6 @@
 const net = require("net");
 class Request {
+    // 在Request构造器中收集必要的信息
     constructor(options) {
         this.method = options.method || "GET";
         this.host = options.host;
@@ -21,12 +22,59 @@ class Request {
         }
         this.headers["Content-Length"] = this.bodyText.length;
     }
-
-    send() {
+    // 设计一个send函数，把请求真实地发送到服务器
+    send(connection) {
         return new Promise((resolve, reject) => {
-            // todo
+            const parser = new ResponseParser();
+            if (connection) {
+                connection.write(this.toString());
+            } else {
+                connection = net.createConnection(
+                    {
+                        host: this.host,
+                        port: this.port,
+                    },
+                    () => {
+                        connection.write(this.toString());
+                    }
+                );
+            }
+
+            connection.on("data", (data) => {
+                console.log(data.toString());
+                // 收到数据传给parser
+                parser.receive(data.toString());
+                // 根据parser的状态去resolve Promise
+                if (parser.isFinished) {
+                    resolve(parser.response);
+                }
+                connection.end();
+            });
+            connection.on("error", (err) => {
+                reject(err);
+                connection.end();
+            });
         });
     }
+
+    toString() {
+        return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers)
+    .map((key) => `${key}: ${this.headers[key]}`)
+    .join("\r\n")}\r
+\r
+${this.bodyText}`;
+    }
+}
+
+class ResponseParser {
+    constructor() {}
+    receive(string) {
+        for (let i = 0; i < string.length; i++) {
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char) {}
 }
 
 void (async function () {
